225ebe5b01138b047735353d39bea415f73f8b17
rafel <rafelbuenog2201@gmail.com>
2026-06-02 14:44:29 -0300

    adicionando portas

--- conexao.py
+++ conexao.py
@@ -5,7 +5,8 @@ def conectar():
         host="acela.proxy.rlwy.net",
         user="root",
         password="cGHlnwhsZwrXZarwFzuNHSUjSjMzLiuC",
-        database="railway"
+        database="railway",
+        port=12056
     )
 
     return conexao
